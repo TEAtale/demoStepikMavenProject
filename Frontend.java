3cc68975a9ebd7c260468a5eb96030f15abf4ca3
TEAtale <ev00genia@gmail.com>
2023-01-06 17:18:10 +0400

    project+dependencies

---
+++ Frontend.java
@@ -0,0 +1,22 @@
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    private final String login = "";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
